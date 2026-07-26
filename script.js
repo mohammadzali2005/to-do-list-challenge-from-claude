3c7a14bdee021ec4ba2e6f873a65be80bca2fcd6
mzking <mzking1384@gmail.com>
2026-07-26 21:52:37 +0300

    added addTask and showTasks functions to add a new task by clicking a button. Removed the extra buttons because one was enough.

--- script.js
+++ script.js
@@ -10,3 +10,42 @@ Requirements
 - If the input is empty and the user tries to add, show a small inline error message instead of adding a blank task
 - A counter at the top shows how many tasks are remaining (uncompleted)
 */
+
+const tasksContainer = document.querySelector('.tasks');
+
+const addBtn = document.querySelector('.addNew-btn');
+const addInput = document.querySelector('.addNew-input');
+
+// All tasks' array
+let tasks = [
+    
+];
+
+// showing the tasks
+function showTasks(){
+    for (let i = 0; i <= tasks.length; i++){
+        tasksContainer.innerHTML = 
+        `<div class="task">
+            <input type="radio" class="task-check">
+            <h3 class="task-text">${tasks[i].text}</h3>
+            <i class="fa-solid fa-trash task-trash-icon"></i>
+          </div>`;
+    }
+}
+
+// adding task
+function addTask(){
+    tasks.push({
+        text: addInput.value
+    });
+}
+
+function removeTask(){
+    
+}
+
+addBtn.addEventListener('click', () => {
+    addTask();
+    showTasks();
+});
+
